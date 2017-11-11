444419f03fe1cb5f49f3d33ad466c8a3ef2eb5e2
Alexander Rodionov <alx1dr@gmail.com>
2017-11-11 23:29:49 +0300

    change books url in accordance with django url styleguide
    - [python - Why django urls end with a slash? - Stack Overflow](https://stackoverflow.com/questions/42212122/why-django-urls-end-with-a-slash)

--- lib/urls.py
+++ lib/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^books$', views.books, name='books'),
+    url(r'^books/$', views.books, name='books'),
     url(r'^books/(?P<book_id>[0-9]+)/$', views.book_detail, name='book_detail'),
 ]
