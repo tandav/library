c7024bb7b763869cd2cf0023283ff043a6f58659
Alexander Rodionov <alx1dr@gmail.com>
2017-11-11 10:34:48 +0300

    make initials models and first migration

--- lib/models.py
+++ lib/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+# book (id, name, author)
+# authors (id, name)
