c1867f029d8ffa9f9ccdc995b54b05df69376c6b
Alexander Rodionov <alx1dr@gmail.com>
2017-11-11 22:20:39 +0300

    better all books MVP

--- lib/urls.py
+++ lib/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',      views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^books$', views.books, name='books'),
+    url(r'^books/(?P<book_id>[0-9]+)/$', views.book_detail, name='book_detail'),
 ]
--- lib/views.py
+++ lib/views.py
@@ -6,9 +6,9 @@ def index(request):
     return HttpResponse('Welcome to the library')
 
 def books(request):
-    all_books = Book.objects.all()
-    output = ', '.join([q.name for q in all_books])
-    return HttpResponse(output)
+    all_books_list = Book.objects.all()
+    context = {'all_books_list': all_books_list}
+    return render(request, 'lib/books.html', context)
 
-# def books(request):
-    # return HttpResponse('List of all books')
+def book_detail(request, book_id):
+    return HttpResponse('Detailed view for book {}'.format(book_id))
