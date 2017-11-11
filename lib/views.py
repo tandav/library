54d0da9aa1a7a02f1769643e12d7aac0a8c10da5
Alexander Rodionov <alx1dr@gmail.com>
2017-11-11 21:15:53 +0300

    all books view MVP

--- lib/views.py
+++ lib/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Author, Book
 
 def index(request):
     return HttpResponse('Welcome to the library')
+
+def books(request):
+    all_books = Book.objects.all()
+    output = ', '.join([q.name for q in all_books])
+    return HttpResponse(output)
+
+# def books(request):
+    # return HttpResponse('List of all books')
