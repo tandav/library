c58c518b51465cc129f643f7cd30c2c4c52ebeb3
Alexander Rodionov <alx1dr@gmail.com>
2017-11-12 00:41:41 +0300

    add authors and author_detail view

--- lib/urls.py
+++ lib/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^books/$', views.books, name='books'),
     url(r'^books/(?P<book_id>[0-9]+)/$', views.book_detail, name='book_detail'),
+    url(r'^authors/$', views.authors, name='authors'),
+    url(r'^authors/(?P<author_id>[0-9]+)/$', views.author_detail, name='author_detail'),
 ]
--- lib/views.py
+++ lib/views.py
@@ -5,6 +5,16 @@ from .models import Author, Book
 def index(request):
     return HttpResponse('Welcome to the library')
 
+def authors(request):
+    all_authors_list = Author.objects.all()
+    context = {'all_authors_list': all_authors_list}
+    return render(request, 'lib/authors.html', context)
+
+def author_detail(request, author_id):
+    author = get_object_or_404(Author, pk=author_id)
+    books = Book.objects.filter(author_id=author_id)
+    return render(request, 'lib/author_detail.html', {'author': author, 'books': books})
+
 def books(request):
     all_books_list = Book.objects.all()
     context = {'all_books_list': all_books_list}
