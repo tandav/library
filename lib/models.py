6175b5aec180b2801f86cbc2dd3fbb3502d347c6
Alexander Rodionov <alx1dr@gmail.com>
2017-11-11 15:16:29 +0300

    add __str__() methods to models

--- lib/models.py
+++ lib/models.py
@@ -4,10 +4,12 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
 
 class Book(models.Model):
     name = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
-# book (id, name, author)
-# authors (id, name)
+    def __str__(self):
+        return self.name
