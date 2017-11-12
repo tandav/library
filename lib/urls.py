3c4ec3a87a444b2fb048664eb8e2594000793f1d
Alexander Rodionov <alx1dr@gmail.com>
2017-11-13 00:02:44 +0300

    Back to function based views

--- lib/urls.py
+++ lib/urls.py
@@ -6,9 +6,9 @@ app_name = 'library'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^books/$', views.books, name='books'),
-    url(r'^books/(?P<pk>[0-9]+)/$', views.BookDetailView.as_view(), name='book_detail'),
+    url(r'^books/(?P<book_id>[0-9]+)/$', views.book_detail, name='book_detail'),
     url(r'^add_book/$', views.add_book, name='add_book'),
     url(r'^authors/$', views.authors, name='authors'),
-    url(r'^authors/(?P<pk>[0-9]+)/$', views.AuthorDetailView.as_view(), name='author_detail'),
+    url(r'^authors/(?P<author_id>[0-9]+)/$', views.author_detail, name='author_detail'),
     url(r'^add_author/$', views.add_author, name='add_author'),
 ]
--- lib/views.py
+++ lib/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse
-from django.views import generic
 from .models import Author, Book
 from .forms import AddBookForm, AddAuthorForm
 
@@ -13,6 +12,12 @@ def authors(request):
     all_authors_list = Author.objects.all()
     context = {'all_authors_list': all_authors_list}
     return render(request, 'lib/authors.html', context)
+
+
+def author_detail(request, author_id):
+    author = get_object_or_404(Author, pk=author_id)
+    books = Book.objects.filter(author_id=author_id)
+    return render(request, 'lib/author_detail.html', {'author': author, 'books': books})
 
 
 def add_author(request):
@@ -31,6 +36,11 @@ def books(request):
     return render(request, 'lib/books.html', context)
 
 
+def book_detail(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    return render(request, 'lib/book_detail.html', {'book': book})
+
+
 def add_book(request):
     if request.method == 'POST':
         form = AddBookForm(request.POST)
@@ -39,20 +49,3 @@ def add_book(request):
             return redirect('library:books')
     form = AddBookForm()
     return render(request, 'lib/book_edit.html', {'form': form})
-
-
-class BookDetailView(generic.DetailView):
-    model = Book
-    template_name = 'lib/book_detail.html'
-
-
-class AuthorDetailView(generic.DetailView):
-    model = Author
-    template_name = 'lib/author_detail.html'
-
-    def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
-        context = super().get_context_data(**kwargs)
-        # Add in a QuerySet of all the books
-        context['books'] = Book.objects.filter(author_id=self.kwargs['pk'])
-        return context        
