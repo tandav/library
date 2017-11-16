efe98d6266446e7ae641b4560a267276a5042871
Alexander Rodionov <alx1dr@gmail.com>
2017-11-16 14:19:04 +0300

    del STATICFILES_DIRS

--- library/settings.py
+++ library/settings.py
@@ -122,10 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
